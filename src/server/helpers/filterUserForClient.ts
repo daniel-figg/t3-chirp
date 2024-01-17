4591b1b8c8d98076314a4605a8627faa1ae219f5
Daniel <daniel.figg@gmail.com>
2024-01-17 11:08:36 -0500

    add filter user

---
+++ src/server/helpers/filterUserForClient.ts
@@ -0,0 +1,12 @@
+import type { User } from "@clerk/nextjs/server";
+export const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    imageUrl: user.imageUrl,
+    externalUsername:
+      user.externalAccounts.find(
+        (externalAccount) => externalAccount.provider === "oauth_github",
+      )?.username || null,
+  };
+};
