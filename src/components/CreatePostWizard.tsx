0c93e94f623b1edc0d9d56cbe83048d34a37cb35
Daniel <daniel.figg@gmail.com>
2024-01-17 12:44:24 -0500

    add create post wiz

---
+++ src/components/CreatePostWizard.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { UserButton, useUser } from "@clerk/nextjs";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { api } from "~/trpc/react";
+import { LoadingSpinner } from "./LoadingSpinner";
+
+export const CreatePostWizard = () => {
+  const { user } = useUser();
+
+  const [input, setInput] = useState("");
+
+  const utils = api.useUtils();
+
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      utils.posts.getAll.invalidate();
+    },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Failed to post! Please try again later.");
+      }
+    },
+  });
+
+  if (!user) return null;
+
+  return (
+    <div className="flex w-full gap-3">
+      <UserButton
+        appearance={{
+          elements: {
+            userButtonAvatarBox: {
+              width: 56,
+              height: 56,
+            },
+          },
+        }}
+      />
+      <input
+        placeholder="Type some emojis!"
+        className="grow bg-transparent outline-none"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            if (input !== "") {
+              mutate({ content: input });
+            }
+          }
+        }}
+        disabled={isPosting}
+      />
+      {input !== "" && !isPosting && (
+        <button onClick={() => mutate({ content: input })}>Post</button>
+      )}
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
+    </div>
+  );
+};
