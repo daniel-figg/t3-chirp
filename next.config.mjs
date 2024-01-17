2d4ddba37dfa9c3422581586e73734afcd31b03b
Daniel <daniel.figg@gmail.com>
2024-01-17 11:50:43 -0500

    add image remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,15 @@
 await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
 
 export default config;
