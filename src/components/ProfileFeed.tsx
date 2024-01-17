f47972150853c83a53980d03d6dfc269211f053f
Daniel <daniel.figg@gmail.com>
2024-01-17 15:33:01 -0500

    create profile feed

---
+++ src/components/ProfileFeed.tsx
@@ -0,0 +1,21 @@
+import { api } from "~/trpc/react";
+import { LoadingPage } from "./LoadingPage";
+import { PostView } from "./PostView";
+
+export const ProfileFeed = (props: { userId: string }) => {
+  const { data, isLoading } = api.posts.getPostsByUserId.useQuery({
+    userId: props.userId,
+  });
+
+  if (isLoading) return <LoadingPage />;
+
+  if (!data || data.length === 0) return <div>User has not posted</div>;
+
+  return (
+    <div className="flex flex-col">
+      {data.map((fullPost) => (
+        <PostView {...fullPost} key={fullPost.post.id} />
+      ))}
+    </div>
+  );
+};
