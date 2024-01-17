918f072fb4ce0769b35d1ec3ab277b3f5c872a3d
Daniel <daniel.figg@gmail.com>
2024-01-17 15:33:11 -0500

    disable overscroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`font-sans ${inter.variable}`}>
+        <body className={`font-sans ${inter.variable} overscroll-none`}>
           <Toaster position="bottom-center" />
           <TRPCReactProvider headers={headers()}>{children}</TRPCReactProvider>
         </body>
